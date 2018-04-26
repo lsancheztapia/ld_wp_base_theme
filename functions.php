063f3559ba8ef31a9d801b65af987cf5ecb65a3c
Luis Sanchez <luis@logicaldesign.pe>
2018-04-26 18:21:35 -0400

    updated

--- functions.php
+++ functions.php
@@ -9,3 +9,17 @@ function registering_navigations() {
    );
  }
  add_action( 'init', 'registering_navigations' );
+
+function registering_widgets_area() {
+        register_sidebar( array(
+                'name'          => __( 'Widget Area', 'custom_theme' ),
+                'id'            => 'sidebar-1',
+                'description'   => __( 'Add widgets here to appear in your sidebar.', 'custom_theme' ),
+                'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+                'after_widget'  => '</aside>',
+                'before_title'  => '<h2 class="widget-title">',
+                'after_title'   => '</h2>',
+        ) );
+}
+add_action( 'widgets_init', 'registering_widgets_area' );
+
