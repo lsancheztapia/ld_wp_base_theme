b503ff057de352367cd65637a4e7b463aace8dcd
Luis Sánchez <luis@logicaldesign.pe>
2018-05-04 10:59:47 -0500

    content.php udpated

--- templates/content.php
+++ templates/content.php
@@ -1,11 +1,18 @@
 <article <?php post_class(); ?>>
 
+  <?php
+    $the_post = get_post();
+  ?>
+
   <h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 
   <?php get_template_part('templates/entry-meta'); ?>
 
   <div>
-    <?php the_content('heheyy'); ?>
+    <?php
+      setup_postdata( $the_post );
+      the_content();
+    ?>
   </div>
 <!--
   <div class="entry-summary">
