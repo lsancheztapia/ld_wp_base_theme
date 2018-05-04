98a969b9ae08449fd545da8c65e506cb43018547
Luis Sánchez <luis@logicaldesign.pe>
2018-05-04 11:01:01 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -23,3 +23,39 @@ function registering_widgets_area() {
 }
 add_action( 'widgets_init', 'registering_widgets_area' );
 
+/**
+ * return posts
+ * attrinbute numberposts
+ * use : [get-last-blog-posts numberposts=5]
+ */
+function get_last_blog_posts( $atts ){
+  $numberposts = 3 ;
+  if ( isset( $atts['numberposts']) && $atts['numberposts'] > 0 ) {
+    $numberposts = $atts['numberposts'];
+  }
+
+  $posts = get_posts(
+    array(
+      'numberposts'   => $numberposts
+    )
+  );
+
+  if ( empty( $posts ) ) { return ''; }
+
+
+  $out = '<ul>';
+  foreach( $posts as $post ) {
+//print_r( wp_trim_words( $post->post_content, 40, '...' ) );
+
+    $title = sprintf( 
+      '<a href="%s" title="%s">%s</a>',
+      get_permalink( $post ),
+      esc_attr( $post->post_title ),
+      esc_html( $post->post_title )
+    ); 
+    $out .= '<li><h2>' . $title . '</h2><div class="summary"><p>' . wp_trim_words( $post->post_content, 40, ' ...' ) . '</p></div></li>';
+  }
+  $out .= '</ul>';
+  return $out;
+}
+add_shortcode( 'get-last-blog-posts', 'get_last_blog_posts' );
