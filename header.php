02df7c47092d6ec885ecb098cd17e199d1a923b7
Luis Sánchez <luis@logicaldesign.pe>
2018-05-05 09:31:32 -0500

    Update header.php

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title><title><?php wp_title(''); ?></title></title>
+  <title><?php wp_title(''); ?></title>
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
   <link href="<?php echo get_bloginfo( 'template_directory' );?>/css/style.css" rel="stylesheet">
 
