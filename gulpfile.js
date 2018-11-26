b1403adf193d45a33e1ba01ee781952b4594d694
Luis Sánchez <luis@logicaldesign.pe>
2018-11-26 15:30:43 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,46 @@
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var chokidar = require('chokidar');
+// var chokidar = require('chokidar');
+
+var timeWait = 200
 
 gulp.task('styles', function() {
-            gulp.src('sass/**/*.scss')
-                .pipe(sass().on('error', sass.logError))
-                .pipe(gulp.dest('./css/'));
+  console.log('compiling')
+  setTimeout(function() {
+    gulp.src('sass/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./css/'));
+    console.log('end')
+  }, timeWait );
+  
 });
 
 //Watch task
 gulp.task('default',function() {
     gulp.watch('sass/**/*.scss',['styles']);
-    });
+});
 
+/*
 gulp.task('watch', function () {
-    var watcher = chokidar.watch('./sass/**/*.scss');
+//    var watcher = chokidar.watch('./sass/*.scss');
+    gulp.start('styles');
 
-    watcher.on('ready', function () {
+
+//    watcher.on('ready', function () {
 
     watcher.on('all', function (e, path) {
+console.log('==============>', e)
+
 
 //       if (e == 'change' && !re_safe_save.test(path))
-       if (e == 'change' )
+       if (e == 'change' ) {
            gulp.start('styles');
+         }
 
-       if (e == 'unlink' && re_safe_save.test(path))
-          gulp.start('styles');
      });
-   })
+
+//   })
+
 });
+*/
